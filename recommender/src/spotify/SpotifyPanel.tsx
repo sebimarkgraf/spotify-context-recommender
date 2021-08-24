c003e52ffdaffa66a31ffa7aaee763b9a0d9f6e6
Sebastian Markgraf <Sebastian-Markgraf@t-online.de>
2021-08-24 14:08:04 +0200

    Restyle search playlist button

--- recommender/src/spotify/SpotifyPanel.tsx
+++ recommender/src/spotify/SpotifyPanel.tsx
@@ -1,8 +1,18 @@
-import { Box, Button, Typography } from "@material-ui/core"
+import { Box, Button, Fab, makeStyles, Typography } from "@material-ui/core"
 import { useState } from "react";
 import { PlaylistPanel, Playlist } from "./PlaylistPanel"
 import { SpotifyRecommender } from "./spotify-recommend";
 
+const useStyles = makeStyles(theme => ({
+    box: {
+        display: "flex",
+        flexDirection: "column",
+        marginTop: theme.spacing(1),
+        alignItems: "center",
+    },
+    button: {
+    }
+}));
 
 interface Props {
     activity: string;
@@ -14,6 +24,7 @@ export const EmptyPanel = () => {
 }
 
 export const SpotifyPanel = (props: Props) => {
+    const classes = useStyles();
     const onClick = (id: number) => console.log(id)
     const recommender = SpotifyRecommender.getInstance();
 
@@ -26,8 +37,10 @@ export const SpotifyPanel = (props: Props) => {
         });
     }
 
-    return (<Box>
-        <Button variant="contained" color="primary" onClick={() => searchPlaylists()}>Search Playlists</Button>
+    return (<Box className={classes.box}>
+         <Button variant="contained" className={classes.button} color="primary" aria-label="search" onClick={() => searchPlaylists()}>
+             Search Playlists
+        </Button>
         {playlists.length === 0 ? <EmptyPanel /> : <PlaylistPanel playlists={playlists} onPlaylistClick={onClick}/>}
     </Box>
     )
